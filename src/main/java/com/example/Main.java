b33c11121f7cd0c7ec93b6e6eba3aba924d2376c
Hariharan <harimaniam@gmail.com>
2024-02-15 22:55:11 +0000

    HH-02-02: Using Lambdas.

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,9 +11,19 @@ public class Main {
         Greeting greeting1 = new Greeting() {
             @Override
             public void sayHello() {
-                System.out.println("Hello Greeting");
+                System.out.println("Hello Greeting1");
             }
         };
         greeting1.sayHello();
+
+        Greeting greeting2 = () -> System.out.println("Hello Greeting2");
+        greeting2.sayHello();
+
+        Calculator calculator = (int x, int y) -> {
+            Random random = new Random();
+            int randomNumber = random.nextInt(50);
+            return x * y + randomNumber;
+        };
+        System.out.println(calculator.calculate(1, 2));
     }
 }
